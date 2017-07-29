e3010190a8f1daa186609571f0194404a9126b2a
git-Andre <post@andre-oelschlaegel.de>
2017-07-29 20:05:48 +0200

    dataBase 1.07

--- src/Providers/PluginAuctionsRouteServiceProvider.php
+++ src/Providers/PluginAuctionsRouteServiceProvider.php
@@ -18,10 +18,10 @@ class PluginAuctionsRouteServiceProvider extends RouteServiceProvider
     {
         $router->get('hallo','PluginAuctions\Controllers\PluginAuctionsController@getHelloWorldPage');
 
-        $router->get('auction', 'PluginAuctions\Controllers\ContentController@showAuction');
-        $router->post('auction', 'PluginAuctions\Controllers\ContentController@createAuction');
-        $router->put('auction/{id}', 'PluginAuctions\Controllers\ContentController@updateAuction')->where('id', '\d+');
-        $router->delete('auction/{id}', 'PluginAuctions\Controllers\ContentController@deleteAuction')->where('id', '\d+');
+        $router->get('auction', 'PluginAuctions\Controllers\PluginAuctionsController@showAuction');
+        $router->post('auction', 'PluginAuctions\Controllers\PluginAuctionsController@createAuction');
+        $router->put('auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@updateAuction')->where('id', '\d+');
+        $router->delete('auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@deleteAuction')->where('id', '\d+');
 
     }
 }
