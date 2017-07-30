2aab199237286fb568b93cd50a5e2e3aea05cb9f
git-Andre <post@andre-oelschlaegel.de>
2017-07-30 19:38:36 +0200

    dataBase 1.31 (neue Routes...)

--- src/Providers/PluginAuctionsRouteServiceProvider.php
+++ src/Providers/PluginAuctionsRouteServiceProvider.php
@@ -1,27 +1,33 @@
 <?php
 
-namespace PluginAuctions\Providers;
+    namespace PluginAuctions\Providers;
 
-use Plenty\Plugin\RouteServiceProvider;
-use Plenty\Plugin\Routing\Router;
+    use Plenty\Plugin\RouteServiceProvider;
+    use Plenty\Plugin\Routing\ApiRouter;
+    use Plenty\Plugin\Routing\Router;
 
-/**
- * Class PluginAuctionsRouteServiceProvider
- * @package PluginAuctions\Providers
- */
-class PluginAuctionsRouteServiceProvider extends RouteServiceProvider
-{
     /**
-     * @param Router $router
+     * Class PluginAuctionsRouteServiceProvider
+     * @package PluginAuctions\Providers
      */
-    public function map(Router $router)
-    {
-        $router->get('hallo','PluginAuctions\Controllers\PluginAuctionsController@getHelloWorldPage');
+    class PluginAuctionsRouteServiceProvider extends RouteServiceProvider {
 
-        $router->get('api/auctions', 'PluginAuctions\Controllers\PluginAuctionsController@showAuctions');
-        $router->post('api/auction', 'PluginAuctions\Controllers\PluginAuctionsController@createAuction');
-        $router->put('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@updateAuction')->where('id', '\d+');
-        $router->delete('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@deleteAuction')->where('id', '\d+');
+        /**
+         * @param Router $router
+         */
+        public function map(Router $router, ApiRouter $apiRouter)
+        {
+            $apiRouter -> version(['v1'], ['namespace' => 'PluginAuctions\Controllers', 'middleware' => 'oauth'],
+                function ($apiRouter) {
 
+                    $router -> get('api/auctions', 'PluginAuctionsController@showAuctions');
+                    $router -> post('api/auction', 'PluginAuctionsController@createAuction');
+                    $router -> put('api/auction/{id}', 'PluginAuctionsController@updateAuction') -> where('id', '\d+');
+                    $router -> delete('api/auction/{id}', 'PluginAuctionsController@deleteAuction') -> where('id', '\d+');
+                });
+
+
+            $router -> get('hallo', 'PluginAuctions\Controllers\PluginAuctionsController@getHelloWorldPage');
+
+        }
     }
-}
