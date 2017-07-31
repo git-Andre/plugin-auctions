85fed4ed42d2438ca2229a44c49c3343884e1061
git-Andre <post@andre-oelschlaegel.de>
2017-07-31 21:06:47 +0200

    dataBase 1.55(neue Routes...)

--- src/Providers/PluginAuctionsRouteServiceProvider.php
+++ src/Providers/PluginAuctionsRouteServiceProvider.php
@@ -18,16 +18,15 @@
          */
         public function map(Router $router, ApiRouter $apiRouter)
         {
-            $apiRouter -> version(['v1'], ['namespace'  => 'PluginAuctions\Controllers', 'middleware' => 'oauth'
-            ],
-                function ($apiRouter) {
+            $apiRouter -> version(['v1'], ['namespace'  => 'PluginAuctions\Controllers', 'middleware' => 'oauth']);
+//                function ($apiRouter) {
 
                     $apiRouter -> get('api/auctions', 'PluginAuctionsController@getAuctions');
                     $apiRouter -> post('api/auction', 'PluginAuctionsController@createAuction');
 //                    $apiRouter -> get('api/auction/{id}', 'PluginAuctionsController@getAuction') -> where('id', '\d+');
 //                    $apiRouter -> put('api/auction/{id}', 'PluginAuctionsController@updateAuction') -> where('id', '\d+');
 //                    $apiRouter -> delete('api/auction/{id}', 'PluginAuctionsController@deleteAuction') -> where('id', '\d+');
-                });
+//                });
             $router -> get('hallo', 'PluginAuctions\Controllers\PluginAuctionsController@getHelloWorldPage');
 
 
