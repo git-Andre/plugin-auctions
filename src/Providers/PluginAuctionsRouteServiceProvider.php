1cde400371d963814498b46d139ea9f65b5d4244
git-Andre <post@andre-oelschlaegel.de>
2017-08-01 14:04:44 +0200

    api Routes

--- src/Providers/PluginAuctionsRouteServiceProvider.php
+++ src/Providers/PluginAuctionsRouteServiceProvider.php
@@ -16,36 +16,34 @@
         /**
          * @param Router $router
          */
-        public function map(Router $router)
+//        public function map(Router $router)
+        public function map(Router $router, ApiRouter $api)
         {
-            $router->get('halt','PluginAuctions\Controllers\PluginAuctionsController@getHelloWorldPage');
+//            $router->get('halt','PluginAuctions\Controllers\PluginAuctionsController@getHelloWorldPage');
 
-            $router->get('api/auctions', 'PluginAuctions\Controllers\PluginAuctionsController@getAuctions');
-            $router->get('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@getAuction')->where('id', '\d+');
-            $router->post('api/auction', 'PluginAuctions\Controllers\PluginAuctionsController@createAuction');
-            $router->put('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@updateAuction')->where('id', '\d+');
-            $router->delete('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@deleteAuction')->where('id', '\d+');
+//            $router->get('api/auctions', 'PluginAuctions\Controllers\PluginAuctionsController@getAuctions');
+//            $router->get('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@getAuction')->where('id', '\d+');
+//            $router->post('api/auction', 'PluginAuctions\Controllers\PluginAuctionsController@createAuction');
+//            $router->put('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@updateAuction')->where('id', '\d+');
+//            $router->delete('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@deleteAuction')->where('id', '\d+');
 
-        }
-//        public function map(Router $router, ApiRouter $api)
-//        {
-////            $api -> version(['v1'], ['namespace' => 'PluginAuctions\Controllers', 'middleware' => 'oauth'],
-////
-////                function ($api) {
-////                    $api -> get('api/auctions', 'PluginAuctionsController@getAuctions');
-////                });
-////
-//            /** @var ApiRouter $routerApi */
-////                    $routerApi -> get('api/auctions', ['uses' => 'PluginAuctions\Controllers\PluginAuctionsController@getAuctions']);
-////                    $routerApi -> post('api/auction', ['uses' => 'PluginAuctions\Controllers\PluginAuctionsController@createAuction']);
+            $api -> version(['v1'], ['namespace' => 'PluginAuctions\Controllers', 'middleware' => 'oauth'],
 //
-////                    $routerApi -> get('api/auction/{id}', 'PluginAuctionsController@getAuction') -> where('id', '\d+');
-////                    $routerApi -> put('api/auction/{id}', 'PluginAuctionsController@updateAuction') -> where('id', '\d+');
-////                    $routerApi -> delete('api/auction/{id}', 'PluginAuctionsController@deleteAuction') -> where('id', '\d+');
-////                });
+                function ($api) {
+                    $api -> get('api/auctions', 'PluginAuctionsController@getAuctions');
+                });
+//
+            /** @var ApiRouter $routerApi */
+//                    $routerApi -> get('api/auctions', ['uses' => 'PluginAuctions\Controllers\PluginAuctionsController@getAuctions']);
+//                    $routerApi -> post('api/auction', ['uses' => 'PluginAuctions\Controllers\PluginAuctionsController@createAuction']);
+
+//                    $routerApi -> get('api/auction/{id}', 'PluginAuctionsController@getAuction') -> where('id', '\d+');
+//                    $routerApi -> put('api/auction/{id}', 'PluginAuctionsController@updateAuction') -> where('id', '\d+');
+//                    $routerApi -> delete('api/auction/{id}', 'PluginAuctionsController@deleteAuction') -> where('id', '\d+');
+//                });
 //            $router -> get('api/auctions', 'PluginAuctions\Controllers\PluginAuctionsController@getAuctions');
 //
 //            $router -> get('hallo', 'PluginAuctions\Controllers\PluginAuctionsController@getHelloWorldPage');
 //
-//        }
+        }
     }
