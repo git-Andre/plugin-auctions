5f720bc6f7c1a2e1f842f2c5db04911c9ca9b2c8
git-Andre <post@andre-oelschlaegel.de>
2017-08-01 15:36:18 +0200

    Auction_1_1

--- src/Migrations/CreateAuctionTable.php
+++ src/Migrations/CreateAuctionTable.php
@@ -1,22 +1,23 @@
 <?php
 
-namespace PluginAuctions\Migrations;
+    namespace PluginAuctions\Migrations;
 
-use PluginAuctions\Models\Auction_1_1;
-use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
+    use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
+    use PluginAuctions\Models\Auction;
+    use PluginAuctions\Models\Auction_1_1;
 
-/**
- * Class CreateAuctionTable
- */
-class CreateAuctionTable
-{
     /**
-     * @param Migrate $migrate
+     * Class CreateAuctionTable
      */
-    public function run(Migrate $migrate)
-    {
-        $migrate->deleteTable(Auction::class);
+    class CreateAuctionTable {
 
-        $migrate->createTable(Auction_1_1::class);
+        /**
+         * @param Migrate $migrate
+         */
+        public function run(Migrate $migrate)
+        {
+            $migrate -> createTable(Auction_1_1::class);
+
+            $migrate -> deleteTable(Auction::class);
+        }
     }
-}
