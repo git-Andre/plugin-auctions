3f05d73e1a3489c5b47ca0001404584988ce6009
git-Andre <post@andre-oelschlaegel.de>
2017-08-10 15:11:53 +0200

    API UI - 1.05 weitere API-Routes Versuche... ['v1'] delete

--- src/Providers/PluginAuctionsRouteServiceProvider.php
+++ src/Providers/PluginAuctionsRouteServiceProvider.php
@@ -15,21 +15,25 @@
         /**
          * @param ApiRouter $api
          */
+
+//    $api->version(['v1'], ['namespace' => 'IO\Api\Resources'], function ($api)
+//        $api->get('io/basket', 'BasketResource@index');
+
 //        public function map(Router $router)
         public function map(Router $router, ApiRouter $api)
         {
-//            $api -> version([''], ['middleware' => ['oauth']],
-//                function ($api) {
-//                    $api -> delete('api/auction/{id}',
-//                            ['uses' => 'PluginAuctions\Controllers\PluginAuctionsController@deleteAuction']);
-//                });
+            $api -> version(['v1'], ['namespace' => 'PluginAuctions\Controllers'], // 'middleware' => ['oauth']
+                function ($api) {
+                    $api -> delete('api/auction/{id}',
+                        ['uses' => 'PluginAuctionsController@deleteAuction']);
+                });
 
 
-            $router->get('api/auctions', 'PluginAuctions\Controllers\PluginAuctionsController@getAuctions');
+            $router -> get('api/auctions', 'PluginAuctions\Controllers\PluginAuctionsController@getAuctions');
             $router -> get('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@getAuction') -> where('id', '\d+');
             $router -> post('api/auction', 'PluginAuctions\Controllers\PluginAuctionsController@createAuction');
             $router -> put('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@updateAuction') -> where('id', '\d+');
-            $router -> delete('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@deleteAuction') -> where('id', '\d+');
+//            $router -> delete('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@deleteAuction') -> where('id', '\d+');
 
 
             /** @var ApiRouter $routerApi TestEbaySdkRouteServiceProvider. */
