3d842c823f143db6e14ca057717e4bcdf71bdd63
git-Andre <post@andre-oelschlaegel.de>
2017-08-10 13:47:14 +0200

    API UI - 1.05 weitere API-Routes Versuche...

--- src/Providers/PluginAuctionsRouteServiceProvider.php
+++ src/Providers/PluginAuctionsRouteServiceProvider.php
@@ -4,31 +4,32 @@
 
     use Plenty\Plugin\RouteServiceProvider;
     use Plenty\Plugin\Routing\ApiRouter;
-    use Plenty\Plugin\Routing\Router;
 
 
     /**
      * Class PluginAuctionsRouteServiceProvider
-     * @package PluginAuctions\Providers
      */
     class PluginAuctionsRouteServiceProvider extends RouteServiceProvider {
 
         /**
-         * @param Router $router
+         * @param ApiRouter $api
          */
 //        public function map(Router $router)
         public function map(Router $router, ApiRouter $api)
         {
+            $api -> version(['v1'], ['middleware' => ['oauth']],
+                function ($api) {
+                    $api -> get('api/auctions/',
+                            ['uses' => 'PluginAuctions\Controllers\PluginAuctionsController@getAuctions']);
+                });
+
+
             $router->get('api/auctions', 'PluginAuctions\Controllers\PluginAuctionsController@getAuctions');
             $router -> get('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@getAuction') -> where('id', '\d+');
             $router -> post('api/auction', 'PluginAuctions\Controllers\PluginAuctionsController@createAuction');
-            $router -> put('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@updateAuction') -> where('id', '\d+');
+//            $router -> put('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@updateAuction') -> where('id', '\d+');
             $router -> delete('api/auction/{id}', 'PluginAuctions\Controllers\PluginAuctionsController@deleteAuction') -> where('id', '\d+');
 
-//            $api -> version(['v1'], ['middleware' => 'oauth'],
-//                function ($api) {
-//                    $api -> get('api/auctions/', 'PluginAuctions\Controllers\PluginAuctionsController@getAuctions');
-//                });
 
             /** @var ApiRouter $routerApi TestEbaySdkRouteServiceProvider. */
 //            $api -> version(['v1'], ['middleware' => ['oauth']], function ($router) {
