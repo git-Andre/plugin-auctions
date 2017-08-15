b7e3af0bcc702d08c338717efaac8b177787c494
git-Andre <post@andre-oelschlaegel.de>
2017-08-15 22:05:05 +0200

    API UI - wieder unsicher... Twig Extension

---
+++ src/Extensions/TwigAuctionsServiceProvider.php
@@ -0,0 +1,64 @@
+<?php //strict
+
+namespace PluginAuctions\Extensions;
+
+use Plenty\Plugin\Templates\Extensions\Twig_Extension;
+use PluginAuctions\Services\Database\AuctionsService;
+
+/**
+ * Provide services and helper functions to twig engine
+ * Class TwigServiceProvider
+ * @package IO\Extensions
+ */
+class TwigAuctionsServiceProvider extends Twig_Extension
+{
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * Return the name of the extension. The name must be unique.
+     *
+     * @return string The name of the extension
+     */
+    public function getName():string
+    {
+        return "PluginAuction_Extension_TwigServiceProvider";
+    }
+
+    /**
+     * Return a list of filters to add.
+     *
+     * @return array The list of filters to add.
+     */
+    public function getFilters():array
+    {
+        return [];
+    }
+
+    /**
+     * Return a list of functions to add.
+     *
+     * @return array the list of functions to add.
+     */
+    public function getFunctions():array
+    {
+        return [];
+    }
+
+
+    /**
+     * Return a map of global helper objects to add.
+     *
+     * @return array the map of helper objects to add.
+     */
+    public function getGlobals():array
+    {
+        return [
+            "auctions" => [
+                "auction"      => pluginApp( AuctionsService::class ),
+            ]
+        ];
+    }
+}
--- src/Providers/PluginAuctionsServiceProvider.php
+++ src/Providers/PluginAuctionsServiceProvider.php
@@ -1,24 +1,28 @@
 <?php
 
-namespace PluginAuctions\Providers;
+    namespace PluginAuctions\Providers;
 
-use Plenty\Plugin\ServiceProvider;
-use PluginAuctions\Contracts\AuctionsRepositoryContract;
-use PluginAuctions\Repositories\AuctionRepository;
+    use Plenty\Plugin\ServiceProvider;
+    use PluginAuctions\Extensions\TwigAuctionsServiceProvider;
 
-/**
- * Class PluginAuctionsServiceProvider
- * @package PluginAuctions\Providers
- */
-class PluginAuctionsServiceProvider extends ServiceProvider
-{
+
     /**
-    * Register the route service provider
-    */
-    public function register()
-    {
-        $this->getApplication()->register(PluginAuctionsRouteServiceProvider::class);
-        $this->getApplication()->bind(AuctionsRepositoryContract::class, AuctionRepository::class);
-    }
+     * Class PluginAuctionsServiceProvider
+     * @package PluginAuctions\Providers
+     */
+    class PluginAuctionsServiceProvider extends ServiceProvider {
 
-}
+        /**
+         * Register the route service provider
+         */
+        public function register()
+        {
+            $this -> getApplication() -> register(PluginAuctionsRouteServiceProvider::class);
+//        $this->getApplication()->bind(AuctionsRepositoryContract::class, AuctionRepository::class);
+        }
+
+        public function boot(Twig $twig)
+        {
+            $twig -> addExtension(TwigAuctionsServiceProvider::class);
+        }
+    }
