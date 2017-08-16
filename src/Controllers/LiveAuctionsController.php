3893a110b3d8331491d6a5fe9c4e4ef3289e9280
git-Andre <post@andre-oelschlaegel.de>
2017-08-17 01:23:28 +0200

    Live Auctions Tabelle etc (wird Auction wieder gelöscht?) last

--- src/Controllers/LiveAuctionsController.php
+++ src/Controllers/LiveAuctionsController.php
@@ -7,91 +7,91 @@
     use PluginAuctions\Services\Database\LiveAuctionsService;
 
     class LiveAuctionsController extends Controller {
-
-        /**
-         * @var AuctionsService
-         */
-        private $liveAuctionsService;
-
-        /**
-         * AuctionsController constructor.
-         * @param AuctionsService $liveAuctionsService
-         */
-        public function __construct(LiveAuctionsService $liveAuctionsService)
-        {
-            $this -> liveAuctionsService = $liveAuctionsService;
-        }
-
-
-        /**
-         * @return array|bool
-         */
-        public function getAuctions()
-        {
-            return $this -> liveAuctionsService -> getAuctions();
-        }
-
-        /**
-         * @param int $auctionId
-         * @return bool|mixed|string
-         */
-        public function getAuction($id)
-        {
-            if ($id && $id > 0)
-            {
-                return json_encode($this -> liveAuctionsService -> getAuction($id));
-            }
-            return 'keine ID (oder 0)';
-        }
-
-        /**
-         * @param Request $request
-         * @return string
-         */
-        public function createAuction(Request $request)
-        {
-            $newLiveAuction = $request -> all();
-
-            if ($newLiveAuction)
-            {
-                if ($this -> liveAuctionsService -> createAuction($newLiveAuction))
-                {
-                    return 'ok';
-                }
-            }
-
-            return 'Fehler beim Request';
-        }
-
-        /**
-         * @param Request $request
-         * @return \PluginAuctions\Services\Database\Auction[]
-         */
-        public function updateAuction(int $id, Request $request)
-        {
-            $auctionData = $request -> all();
-
-            return $this -> liveAuctionsService -> updateAuction($id, $auctionData);
-        }
-
-        /**
-         * @param $auctionId
-         * @return string
-         */
-        public function deleteAuction($id)
-        {
-
-            if ($id)
-            {
-                if ($this -> liveAuctionsService -> deleteAuction($id))
-                {
-                    return 'ok';  //$this -> getAuctions();  // was soll wirklich zurück ???
-                }
-
-                return 'vom LiveAuctionsService kam nichts';
-            }
-
-            return 'keine Id';
-        }
-
+//
+//        /**
+//         * @var AuctionsService
+//         */
+//        private $liveAuctionsService;
+//
+//        /**
+//         * AuctionsController constructor.
+//         * @param AuctionsService $liveAuctionsService
+//         */
+//        public function __construct(LiveAuctionsService $liveAuctionsService)
+//        {
+//            $this -> liveAuctionsService = $liveAuctionsService;
+//        }
+//
+//
+//        /**
+//         * @return array|bool
+//         */
+//        public function getAuctions()
+//        {
+//            return $this -> liveAuctionsService -> getAuctions();
+//        }
+//
+//        /**
+//         * @param int $auctionId
+//         * @return bool|mixed|string
+//         */
+//        public function getAuction($id)
+//        {
+//            if ($id && $id > 0)
+//            {
+//                return json_encode($this -> liveAuctionsService -> getAuction($id));
+//            }
+//            return 'keine ID (oder 0)';
+//        }
+//
+//        /**
+//         * @param Request $request
+//         * @return string
+//         */
+//        public function createAuction(Request $request)
+//        {
+//            $newLiveAuction = $request -> all();
+//
+//            if ($newLiveAuction)
+//            {
+//                if ($this -> liveAuctionsService -> createAuction($newLiveAuction))
+//                {
+//                    return 'ok';
+//                }
+//            }
+//
+//            return 'Fehler beim Request';
+//        }
+//
+//        /**
+//         * @param Request $request
+//         * @return \PluginAuctions\Services\Database\Auction[]
+//         */
+//        public function updateAuction(int $id, Request $request)
+//        {
+//            $auctionData = $request -> all();
+//
+//            return $this -> liveAuctionsService -> updateAuction($id, $auctionData);
+//        }
+//
+//        /**
+//         * @param $auctionId
+//         * @return string
+//         */
+//        public function deleteAuction($id)
+//        {
+//
+//            if ($id)
+//            {
+//                if ($this -> liveAuctionsService -> deleteAuction($id))
+//                {
+//                    return 'ok';  //$this -> getAuctions();  // was soll wirklich zurück ???
+//                }
+//
+//                return 'vom LiveAuctionsService kam nichts';
+//            }
+//
+//            return 'keine Id';
+//        }
+//
     }
