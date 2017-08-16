e24067d8725ad587845c658284627250dc0c8279
git-Andre <post@andre-oelschlaegel.de>
2017-08-16 23:09:58 +0200

    Live Auctions Tabelle etc

--- src/Migrations/CreateAuctionTables.php
+++ src/Migrations/CreateAuctionTables.php
@@ -4,11 +4,13 @@
 
     use Plenty\Modules\Plugin\DataBase\Contracts\Migrate;
     use PluginAuctions\Models\Auction_4;
+    use PluginAuctions\Models\LiveAuction_1;
+
 
     /**
      * Class CreateAuction4Table
      */
-    class CreateAuction4Table {
+    class CreateAuctionTables {
 
         /**
          * @param Migrate $migrate
@@ -16,9 +18,19 @@
         public function run(Migrate $migrate)
         {
             try {
-                $migrate -> createTable(Auction_4::class);
+                $migrate -> deleteTable(Auction_4::class);
             } catch ( \Exception $e ) {
                 echo $e -> getMessage();
             }
+            $migrate -> createTable(Auction_4::class);
+
+
+            try {
+                $migrate -> deleteTable(LiveAuction_1::class);
+            } catch ( \Exception $e ) {
+                echo $e -> getMessage();
+            }
+            $migrate -> createTable(LiveAuction_1::class);
+
         }
     }
---
+++ src/Models/LiveAuction_1.php
@@ -0,0 +1,30 @@
+<?php
+
+    namespace PluginAuctions\Models;
+
+    use Plenty\Modules\Plugin\DataBase\Contracts\Model;
+
+    /**
+     * Class LiveAuction_1
+     * @package PluginAuctions\Models
+     */
+    class LiveAuction_1 extends Model {
+        public $id = 0;
+        public $createdAt = 0;
+        public $itemId = 0;
+        public $auctionId = 0;
+
+        public $bidderList = [];
+
+        public $isEnded = false;
+        public $isLive = false;
+        public $isEndedWithBuyNow = false;
+
+    /**
+     * @return string
+     */
+    public function getTableName() : string
+    {
+        return 'PluginAuctions::LiveAuction_1';
+    }
+}
