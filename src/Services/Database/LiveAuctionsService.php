2b44feaeba96f747ac8747c28f1f5582dea3dbae
git-Andre <post@andre-oelschlaegel.de>
2017-08-19 16:25:11 +0200

    LiveAuction isEnded & is Live vom Server...

--- src/Services/Database/LiveAuctionsService.php
+++ src/Services/Database/LiveAuctionsService.php
@@ -57,12 +57,12 @@ use PluginAuctions\Services\Database\AuctionsService;
                     $isEnded = false;
                     $isLive = true;
 
-                    $auction[] = $this -> auctionService -> getAuctionForItemId($itemId);
+                    $auction[] = json_encode($this -> auctionService -> getAuctionForItemId($itemId));
 
 
-                    $startDate = $auction[0] ['startDate'];
+                    $startDate = $auction[0] -> startDate;
                     if ($auction[0]){
-                        return $auction[0] -> id;
+                        return $auction[0];
                     }
 
                     $startDate = $startDate + ($auction[0] -> startHour * 60 *60) + ($auction[0] -> startMinute *60);
