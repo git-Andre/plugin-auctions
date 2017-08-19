7af83cff7040d6c38faf5163f5730146568c64fc
git-Andre <post@andre-oelschlaegel.de>
2017-08-19 21:09:23 +0200

    LiveAuction isEnded & is Live vom Server...

--- src/Services/Database/LiveAuctionsService.php
+++ src/Services/Database/LiveAuctionsService.php
@@ -68,11 +68,11 @@
                     if ($auction[0])
                     {
 //                        return "start: $startDate - end:  $endDate  - now: $now - hour: $startHour - Minute: $startMinute - Dauer: $auctionDuration";
-                        return $now - $startDate;
+//                        return $now - $startDate;
                     }
 
-                    ($now - $startDate < 0) ? $isLive = true : $isLive = false;
-                    ($now - $endDate > 0) ? $isEnded = true : $isEnded = false;
+                    ($now - $startDate >= 0) ? $isLive = true : $isLive = false;
+                    ($now - $endDate >= 0) ? $isEnded = true : $isEnded = false;
 
                     $liveAuction[0] -> isLive = $isLive;
                     $liveAuction[0] -> isEnded = $isEnded;
