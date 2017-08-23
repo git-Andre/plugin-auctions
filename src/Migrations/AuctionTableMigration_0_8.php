18c678d2b567280dc4c0d9563b3b252f08a0b4ad
git-Andre <post@andre-oelschlaegel.de>
2017-08-23 18:48:49 +0200

    tense eingeführt calcTense

--- src/Migrations/AuctionTableMigration_0_8.php
+++ src/Migrations/AuctionTableMigration_0_8.php
@@ -18,24 +18,6 @@
         {
             try
             {
-                $migrate -> deleteTable(Auction_5::class);
-            }
-            catch ( \Exception $e )
-            {
-                echo $e -> getMessage();
-            }
-
-            try
-            {
-                $migrate -> deleteTable(Auction_6::class);
-            }
-            catch ( \Exception $e )
-            {
-                echo $e -> getMessage();
-            }
-
-            try
-            {
                 $migrate -> deleteTable(Auction_7::class);
             }
             catch ( \Exception $e )
