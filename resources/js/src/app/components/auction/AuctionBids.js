5c7efb4f91f2b7adf9bb7f4dd4d0aedd849c87d3
git-Andre <post@andre-oelschlaegel.de>
2017-08-29 03:35:36 +0200

    Neubeginn Auction ohne NodeJS - test root funktioniert (Bsp. Bob3)

--- resources/js/src/app/components/auction/AuctionBids.js
+++ resources/js/src/app/components/auction/AuctionBids.js
@@ -17,4 +17,27 @@
 //             },
 //
 //         })
-<div>My name is {{name}} and I'm {{age}} years old.<input v-model="name"><input v-model="age"></div>
+
+`<!--<div>My name is {{name}} and I'm {{age}} years old.<input v-model="name"><input v-model="age"></div>-->`
+
+// const ApiService      = require("services/ApiService");
+// const ResourceService = require("services/ResourceService");
+
+Vue.component("auction-test", {
+
+    props: [
+        "template",
+    ],
+
+    data()
+    {
+        return {
+        };
+    },
+
+    created()
+    {
+        this.$options.template = this.template;
+    },
+
+});
--- resources/js/src/app/main.js
+++ resources/js/src/app/main.js
@@ -1,3 +1,6 @@
+const AuctionTest = require("components/auction/AuctionBids");
+
+
 Vue.component('auction-bids', {
     template: `<div>My name is {{name}} and I'm {{age}} years old.<input v-model="name"><input v-model="age"></div>`,
     data() {
@@ -12,8 +15,10 @@ Vue.component('auction-bids', {
 
 new Vue({
             el: '#addAuctionVue',
+            components: {
+              'auction-test': AuctionTest
+            },
             data: {
-
             },
 
         })
