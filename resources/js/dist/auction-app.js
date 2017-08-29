1db3c6fcf92bc5c9654212364ccf52c94e5d9911
git-Andre <post@andre-oelschlaegel.de>
2017-08-29 16:04:26 +0200

    Neubeginn Auction tense neu,

--- resources/js/dist/auction-app.js
+++ resources/js/dist/auction-app.js
@@ -6,7 +6,8 @@
 
 Vue.component("auction-bids", {
     name: 'auctionbids',
-    template: "<div>My name is {{name}} and I'm {{age}} years old.<input v-model=\"name\"><input v-model=\"age\"></div>",
+    template: "\n    <div class=\"col-lg-8 offset-lg-2\" formGroup=\"maxBid\">\n        <p>{{name}} and I'm {{age}} years old</p>\n        <input class=\"form-control form-control-lg text-muted\"\n                type=\"number\"\n                id=\"maximumBid\"\n                placeholder=\"Ihr Maximalgebot\"\n                aria-describedby=\"maxBidHelpBlock\">\n        <p id=\"maxBidHelpBlock\" class=\"form-text text-muted text-center\"> Bitte geben Sie\n                                                                          mindestens \n                                                                          EUR ein!</p>\n        <button class=\"btn btn-primary btn-lg btn-block\" type=\"submit\">Gebot abgeben</button>\n    </div>\n    \n    ",
+
     data: function data() {
         return {
             name: "Bob",
@@ -16,6 +17,19 @@ Vue.component("auction-bids", {
 
     methods: {}
 });
+// Vue.component("auction-bids", {
+//     name: 'auctionbids',
+//     template: "<div>My name is {{name}} and I'm {{age}} years old.<input v-model=\"name\"><input v-model=\"age\"></div>",
+//     data()
+//     {
+//         return {
+//             name: "Bob",
+//             age: 22
+//         };
+//     },
+//     methods: {
+//     }
+// });
 
 },{}],2:[function(require,module,exports){
 "use strict";
--- resources/js/src/app/components/auction/AuctionBids.js
+++ resources/js/src/app/components/auction/AuctionBids.js
@@ -4,7 +4,21 @@
 
 Vue.component("auction-bids", {
     name: 'auctionbids',
-    template: "<div>My name is {{name}} and I'm {{age}} years old.<input v-model=\"name\"><input v-model=\"age\"></div>",
+    template: `
+    <div class="col-lg-8 offset-lg-2" formGroup="maxBid">
+        <p>{{name}} and I'm {{age}} years old</p>
+        <input class="form-control form-control-lg text-muted"
+                type="number"
+                id="maximumBid"
+                placeholder="Ihr Maximalgebot"
+                aria-describedby="maxBidHelpBlock">
+        <p id="maxBidHelpBlock" class="form-text text-muted text-center"> Bitte geben Sie
+                                                                          mindestens 
+                                                                          EUR ein!</p>
+        <button class="btn btn-primary btn-lg btn-block" type="submit">Gebot abgeben</button>
+    </div>
+    `
+    ,
     data()
     {
         return {
@@ -15,3 +29,16 @@ Vue.component("auction-bids", {
     methods: {
     }
 });
+// Vue.component("auction-bids", {
+//     name: 'auctionbids',
+//     template: "<div>My name is {{name}} and I'm {{age}} years old.<input v-model=\"name\"><input v-model=\"age\"></div>",
+//     data()
+//     {
+//         return {
+//             name: "Bob",
+//             age: 22
+//         };
+//     },
+//     methods: {
+//     }
+// });
