6d536d30dff68ef4c597f0e6789ddeda96b81b73
git-Andre <post@andre-oelschlaegel.de>
2017-08-30 14:23:04 +0200

    anfang vue data

--- resources/js/src/app/components/auction/AuctionBids.js
+++ resources/js/src/app/components/auction/AuctionBids.js
@@ -4,32 +4,36 @@
 Vue.component( "auction-bids", {
     name: 'auctionbids',
     template: `
-    <div class="root-component-auction">
-        <dl class="auctionList">
-            <div class="m-t-0">
-                <dt>Restzeit:</dt>
-                <dd class="countDown">{{ remainingTime }}</dd>
-            </div>
-        </dl>
-        <div class="col-lg-8 offset-lg-2" formGroup="maxBid">
-             <input class="form-control form-control-lg text-muted"
-                    type="number"
-                    id="maximumBid"
-                    placeholder="Ihr Maximalgebot"
-                    aria-describedby="maxBidHelpBlock"
-                    >
-            <p id="maxBidHelpBlock" class="form-text text-muted text-center"> Bitte geben Sie
-                                                                              mindestens 
-                                                                              EUR ein!</p>
-            <button class="btn btn-primary btn-lg btn-block" type="submit">Gebot abgeben</button>
-        </div>
+<div class="root-component-auction m-t-0 m-b-2">
+    <div class="m-b-1">
+        <h4>Restzeit: <span class="countDown">{{ remainingTime }}</span></h4>
     </div>
+    <div class="col-lg-8 offset-lg-2" formGroup="maxBid">
+        <input class="form-control form-control-lg text-muted"
+                type="number"
+                id="maximumBid"
+                placeholder="Ihr Maximalgebot"
+                aria-describedby="maxBidHelpBlock"
+        >
+        <p id="maxBidHelpBlock" class="form-text text-muted text-center"> Bitte geben Sie mindestens {{ minBid }} EUR ein!</p>
+        <button class="btn btn-primary btn-lg btn-block" type="submit">Gebot abgeben</button>
+    </div>
+</div>
     `
     ,
+    props: [
+        "auction",
+    ],
     data() {
         return {
-            remainingTime: 'Zeit wird berechnet... (in Kürze... :) )'
+            remainingTime = this.now(),
+            minBid = auction.id
         };
     },
-    methods: {}
+    methods: {},
+    computed: {
+        now: function () {
+            return Date.now()
+        }
+    }
 } );
