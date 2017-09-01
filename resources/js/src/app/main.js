0947bbd85455a22b1ea65433bb44d5b7984c9918
git-Andre <post@andre-oelschlaegel.de>
2017-09-01 12:45:25 +0200

    auction-bids Start 1.1 countdown 1

--- resources/js/src/app/main.js
+++ resources/js/src/app/main.js
@@ -1,6 +1,6 @@
-Vue.filter('twoDigits', (value) => {
-    if ( value.toString().length <= 1 ) {
-        return '0'+value.toString()
-    }
-    return value.toString()
-})
+// Vue.filter('twoDigits', (value) => {
+//     if ( value.toString().length <= 1 ) {
+//         return '0'+value.toString()
+//     }
+//     return value.toString()
+// })
