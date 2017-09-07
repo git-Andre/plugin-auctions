f3951e9d1ed8dfe9651b7ce1d0ad7ed69760e0b1
git-Andre <post@andre-oelschlaegel.de>
2017-09-07 18:36:02 +0200

    Bieterliste fertig ohne Formatierung

--- resources/js/dist/auction-app.js
+++ resources/js/dist/auction-app.js
@@ -151,40 +151,12 @@ Vue.component("auction-show-bidderlist", {
 
             this.bidderList.push(bidView);
         }
-
-        // this.bidderdata = [];
-        // this.bidderList.reverse();
+        this.bidderdata = [];
     },
-    ready: function ready() {
-        // this.changeTooltipText();
-    },
+    ready: function ready() {},
 
 
-    methods: {
-        showBidderList: function showBidderList() {
-            alert('show Bidder List...');
-
-            // if ( this.item.filter.isSalable ) {
-            //     const basketObject =
-            //               {
-            //                   variationId: this.variationId,
-            //                   quantity: this.quantity,
-            //                   basketItemOrderParams: this.item.properties
-            //               };
-            //
-            //     ResourceService.getResource( "basketItems" ).push( basketObject )
-            //         .done( function () {
-            //             this.openAddToBasketOverlay();
-            //         }
-            //                    .bind( this ) )
-            //         .fail( function (response) {
-            //             NotificationService.error(
-            //                 Translations.Template[ExceptionMap.get( response.data.exceptionCode.toString() )] )
-            //                 .closeAfter( 5000 );
-            //         } );
-            // }
-        }
-    }
+    methods: {}
 });
 
 },{"services/NotificationService":6,"services/ResourceService":7}],3:[function(require,module,exports){
