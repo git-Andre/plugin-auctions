c2a3f6cbef976f428f1cc645d7559f2edd21ab62
git-Andre <post@andre-oelschlaegel.de>
2017-09-09 17:21:43 +0200

    headers nach Modal...

--- resources/js/src/app/components/auction/AuctionShowBidderList.js
+++ resources/js/src/app/components/auction/AuctionShowBidderList.js
@@ -13,7 +13,7 @@ Vue.component( "auction-show-bidderlist", {
     data() {
         return {
             bidderList: [],
-            expiryDate: 0,
+            // expiryDate: 0,
             isAuctionPresent: false,
             bidders: 0
         };
@@ -50,24 +50,24 @@ Vue.component( "auction-show-bidderlist", {
                     alert( 'error4: ' + error.toString() );
                 }
             );
-        AuctionBidderService.getExpiryDate( this.auctionid )
-            .then(
-                response => {
-
-                    this.expiryDate = response;
-
-                    if ( Math.trunc( (new Date()).getTime() / 1000 ) < this.expiryDate ) {
-                        this.isAuctionPresent = true;
-                    }
-                    else {
-                        this.isAuctionPresent = false;
-                    }
-                    ;
-                },
-                error => {
-                    alert( 'error5: ' + error.toString() );
-                }
-            );
+        // AuctionBidderService.getExpiryDate( this.auctionid )
+        //     .then(
+        //         response => {
+        //
+        //             this.expiryDate = response;
+        //
+        //             if ( Math.trunc( (new Date()).getTime() / 1000 ) < this.expiryDate ) {
+        //                 this.isAuctionPresent = true;
+        //             }
+        //             else {
+        //                 this.isAuctionPresent = false;
+        //             }
+        //             ;
+        //         },
+        //         error => {
+        //             alert( 'error5: ' + error.toString() );
+        //         }
+        //     );
     },
 
     ready() {
