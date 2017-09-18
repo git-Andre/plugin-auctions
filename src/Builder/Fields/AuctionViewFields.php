de076bec8348f2a0ff64068844ff6513a7217fb5
git-Andre <post@andre-oelschlaegel.de>
2017-09-18 16:08:09 +0200

    Umzug Bids auf Server 1.1 button end raus

---
+++ src/Builder/Fields/AuctionViewFields.php
@@ -0,0 +1,13 @@
+<?php //strict
+
+namespace IO\Builder\Item\Fields;
+
+/**
+ * Class ItemDescriptionFields
+ * @package IO\Builder\Item\Fields
+ */
+class AuctionViewFields
+{
+	const ITEM_ID             = "id";
+	const CHARACTER_CACHE     = "characterCache";
+}
