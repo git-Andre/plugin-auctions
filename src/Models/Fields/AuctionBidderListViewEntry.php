417946b55297f94e6adc30080d68fa619ce37e73
git-Andre <post@andre-oelschlaegel.de>
2017-09-18 17:53:20 +0200

    Umzug Bids auf Server 1.1 get auctions

---
+++ src/Models/Fields/AuctionBidderListViewEntry.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace PluginAuctions\Models\Fields;
+
+    /**
+     * Class ItemBaseFields
+     * @package IO\Builder\Item\Fields
+     */
+    class AuctionBidderListViewEntry {
+
+
+        public $bidderName     = 'Startpreis';
+        public $bidPrice       = 0.1;
+        public $bidTimeStamp   = 0;
+        public $bidStatus      = "start";
+    }
