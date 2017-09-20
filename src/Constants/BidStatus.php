0f9478cceddf4ff21387543daf300dff3c0be539
git-Andre <post@andre-oelschlaegel.de>
2017-09-20 23:11:41 +0200

    bid status format

--- src/Constants/BidStatus.php
+++ src/Constants/BidStatus.php
@@ -10,8 +10,8 @@
 
         const OWN_BID_CHANGED   = "eigenes Max-Gebot geändert";
         const HIGHEST_BID       = "höchstes Gebot abgegeben";
-        const LOWER_BID         = "es liegt höheres Gebot vor";
-        const START             = "start";
+        const LOWER_BID         = "es lag ein höheres Gebot vor";
+        const START             = "Auktions Start";
 //	const OWN_BID_CHANGED = "ownMaxBidChanged";
 //	const HIGHEST_BID = "highestBid";
 //	const LOWER_BID = "lowerBid";
