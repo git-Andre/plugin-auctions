9d830da611095a557af7abaacaeb488d265b2c3f
git-Andre <post@andre-oelschlaegel.de>
2017-09-20 23:11:41 +0200

    bid status format

--- resources/js/src/app/constants/AuctionConstants.js
+++ resources/js/src/app/constants/AuctionConstants.js
@@ -5,8 +5,8 @@ class AuctionConstants {
 // bidStatus
 export const OWN_BID_CHANGED = "eigenes Max-Gebot geändert";
 export const HIGHEST_BID     = "höchstes Gebot abgegeben";
-export const LOWER_BID       = "es liegt höheres Gebot vor";
-export const START           = "start";
+export const LOWER_BID       = "es lag ein höheres Gebot vor";
+export const START           = "Auktions Start";
 
         // export const OWN_BID_CHANGED = "ownBidChanged";
         // export const HIGHEST_BID     = "highestBid";
