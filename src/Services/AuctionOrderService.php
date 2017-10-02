cc35c4846b77a57765c71a11196d539c9b80b68a
git-Andre <post@andre-oelschlaegel.de>
2017-10-02 18:32:42 +0200

    test place order placeOrder2

--- src/Services/AuctionOrderService.php
+++ src/Services/AuctionOrderService.php
@@ -78,7 +78,7 @@
                 -> withContactId(7076)
                 -> withAddressId(41656, AddressType::BILLING)
                 -> withAddressId(43688, AddressType::DELIVERY)
-                -> withOrderProperty(OrderPropertyType::PAYMENT_METHOD, OrderOptionSubType::MAIN_VALUE, $checkoutService -> getMethodOfPaymentId())
+                -> withOrderProperty(OrderPropertyType::PAYMENT_METHOD, OrderOptionSubType::MAIN_VALUE, 6000)
                 -> withOrderProperty(OrderPropertyType::SHIPPING_PROFILE, OrderOptionSubType::MAIN_VALUE, 34)
                 -> done()
             ;
