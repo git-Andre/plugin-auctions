a95fe38d4b991735d603509511fc2a7590501251
git-Andre <post@andre-oelschlaegel.de>
2017-10-05 23:50:37 +0200

    place Order eigene Builder Params next day (int)

--- src/Builder/AuctionOrderItemBuilder.php
+++ src/Builder/AuctionOrderItemBuilder.php
@@ -36,7 +36,7 @@
             return [
                 "typeId"            => OrderItemType::VARIATION,
                 "referrerId"        => 1, // Mandant Shop ???
-                "itemVariationId"   => (int)38443, // (int)$auctionParams['variationId'],  // 38443
+                "itemVariationId"   => (int)$auctionParams['itemVariationId'],  // 38443
                 "quantity"          => 1, // bei Auktionen immer nur 1
                 "orderItemName"     => $auctionParams['orderItemName'],
                 "shippingProfileId" => 34, // Todo config ??? Standard für Auktionen
