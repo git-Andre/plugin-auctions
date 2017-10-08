b3fb1deedc8f8c50d466462079c6e9ed16d65f2e
git-Andre <post@andre-oelschlaegel.de>
2017-10-09 00:45:25 +0200

    neuer Versuch 212

--- src/Controllers/AuctionPlaceOrderController.php
+++ src/Controllers/AuctionPlaceOrderController.php
@@ -3,10 +3,9 @@
 
 //    use IO\Services\NotificationService;
     use IO\Controllers\LayoutController;
+    use Plenty\Plugin\Http\Request;
+    use Plenty\Plugin\Http\Response;
     use PluginAuctions\Services\AuctionOrderService;
-
-    //use Plenty\Plugin\Http\Response;
-//use Plenty\Plugin\Http\Request;
 
     /**
      * Class AuctionPlaceOrderController
@@ -15,55 +14,61 @@
     class AuctionPlaceOrderController extends LayoutController {
 
         private $orderService;
+        private $request;
+        private $response;
 
         public function __construct(
-            AuctionOrderService $orderService
+            AuctionOrderService $orderService,
+            Request $request,
+            Response $response
         )
         {
             $this -> orderService = $orderService;
+            $this -> request = $request;
+            $this -> response = $response;
         }
 
-        public function
-        triggerPlaceOrder($auctionId)
-        {
-            try
-            {
-                $result = $this -> placeOrder($auctionId);
-                return $result;
-            }
-            catch (\Exception $exception )
-            {
-                return $exception -> getMessage(); // $response->redirectTo("checkout");
-            }
-        }
+//        public function
+//        triggerPlaceOrder($auctionId)
+//        {
+//            try
+//            {
+//                $result = $this -> placeOrder($auctionId);
+//                return $result;
+//            }
+//            catch (\Exception $exception )
+//            {
+//                return $exception -> getMessage(); // $response->redirectTo("checkout");
+//            }
+//        }
+//
+//        public function getOrderById(AuctionOrderService $orderService, int $orderId)
+//        {
+//            $orderData = $this -> orderService -> findOrderById($orderId);
+//
+//            return $orderData;
+//        }
+//
+//        /**
+//         * @param $auctionId
+//         * @return \IO\Models\LocalizedOrder|string
+//         */
+//        private function placeOrder($auctionId)
+//        {
+//            try
+//            {
+//                $orderData = $this -> orderService -> placeOrder($auctionId); // helper für http-Trigger
+//
+////                return "redirectTo("; // $response->redirectTo( "execute-payment/" . $orderData->order->id . (strlen($redirectParam) ? "/?redirectParam=" . $redirectParam : '') );
+//                return $orderData;
+//            }
+//            catch ( \Exception $exception )
+//            {
+//                return $exception -> getMessage(); // $response->redirectTo("checkout");
+//            }
+//        }
 
-        public function getOrderById(AuctionOrderService $orderService, int $orderId)
-        {
-            $orderData = $this -> orderService -> findOrderById($orderId);
-
-            return $orderData;
-        }
-
-        /**
-         * @param $auctionId
-         * @return \IO\Models\LocalizedOrder|string
-         */
-        private function placeOrder($auctionId)
-        {
-            try
-            {
-                $orderData = $this -> orderService -> placeOrder($auctionId); // helper für http-Trigger
-
-//                return "redirectTo("; // $response->redirectTo( "execute-payment/" . $orderData->order->id . (strlen($redirectParam) ? "/?redirectParam=" . $redirectParam : '') );
-                return $orderData;
-            }
-            catch ( \Exception $exception )
-            {
-                return $exception -> getMessage(); // $response->redirectTo("checkout");
-            }
-        }
-
-        public function createOrder(Request $request, Response $response) //: Response
+        public function createOrder() //: Response
         {
             $auctionId = (int) $this -> request -> get("auctionid");
 
@@ -73,22 +78,24 @@
 //
 //                return $this -> response -> create($order, ResponseCode::OK);
 //                return $this -> response -> create($auctionId, ResponseCode::EXPECTATION_FAILED);
-                return $response -> json($request);
+                return $this -> response -> json($this -> request);
             }
 
-            return $response -> json($auctionId);
+            return $this -> response -> json($auctionId);
 
 
         }
-        public function index(Request $request, Response $response) //: Response
+
+        public function index() //: Response
         {
             $auctionId = (int) $this -> request -> get("auctionid");
 
             if ($auctionId > 0)
             {
-                return $response -> json($request);
+                return $this -> response -> json($this -> request);
             }
-            return $response -> make($auctionId);
+
+            return $this -> response -> make($auctionId);
         }
 
     }
