e7aa063e76b29786fcba14d436b1d258e0dc6019
git-Andre <post@andre-oelschlaegel.de>
2017-10-14 18:32:44 +0200

    cron order 2 tense33

--- src/Providers/PluginAuctionsRouteServiceProvider.php
+++ src/Providers/PluginAuctionsRouteServiceProvider.php
@@ -102,7 +102,7 @@
                     -> where('customerId', '\d+');
             $router -> get('api/testparamsbuilder/{auctionId}', 'PluginAuctions\Controllers\AuctionHelperController@auctionParamsBuilder')
                     -> where('auctionId', '\d+');
-            $router -> get('api/test-tense/{tense}', 'PluginAuctions\Controllers\AuctionsController@getAuctionsForTense');
+            $router -> get('api/test-tense', 'PluginAuctions\Controllers\AuctionsController@getAuctionsForTense');
 
             // auctionend - add to basket
             $router->post('auction_to_basket', 'PluginAuctions\Controllers\AuctionToBasketController@add');
