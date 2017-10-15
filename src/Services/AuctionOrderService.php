da2de320a2e5f2c60e9bfd803bbb2e505fc2be52
git-Andre <post@andre-oelschlaegel.de>
2017-10-16 01:00:34 +0200

    order nur bei isAvailable mit logger2

--- src/Services/AuctionOrderService.php
+++ src/Services/AuctionOrderService.php
@@ -45,7 +45,7 @@
                   -> setReferenceValue($auctionId)
                   -> debug('PluginAuctions::auctions.debug', ['$auctionParams: ' => $auctionParams]);
 
-            if ($this -> auctionParams['isSalableAndActive'])
+            if ($auctionParams['isSalableAndActive'])
             {
 
                 $order = pluginApp(AuctionOrderBuilder::class)
