b92da3f4a77c79e53455d27735093ebe7ecc1e3a
git-Andre <post@andre-oelschlaegel.de>
2017-10-21 16:04:29 +0200

    $sessionStorageService pluginApp

--- src/Containers/SingleItemAuctionContainer.php
+++ src/Containers/SingleItemAuctionContainer.php
@@ -7,20 +7,22 @@
 
     class SingleItemAuctionContainer {
 
-        private $sessionStorageService;
-
-        public function __construct(SessionStorageService $sessionStorageService)
-        {
-            $this -> sessionStorageService = $sessionStorageService;
-        }
-
+//        private $sessionStorageService;
+//
+//        public function __construct(SessionStorageService $sessionStorageService)
+//        {
+//            $this -> sessionStorageService = $sessionStorageService;
+//        }
+//
         public function call(Twig $twig, $arg) : string
         {
             $itemId = $arg[0]['item']['id'];
             // sessionstorage (Service) abfragen
+            $session = pluginApp(SessionStorageService::class);
 
-            $visitorCounterTest = $this -> sessionStorageService -> getLang(); // test
-//            $visitorCounter = $this -> sessionStorageService -> getSessionValue("testCounter"); // test
+//            $visitorCounter = $this -> session -> getLang(); // test
+            $visitorCounter = $session -> getSessionValue("testCounter"); // test
+
             return $twig -> render('PluginAuctions::Containers.SingleItemAuction', ["itemData"       => $arg[0],
                                                                                     "visitorCounter" => $visitorCounter
             ]);
