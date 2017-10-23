96fad5def4f37ff94357b37522931bbba02b90f7
git-Andre <post@andre-oelschlaegel.de>
2017-10-23 02:09:50 +0200

    session new architectur

--- src/Containers/SingleItemAuctionContainer.php
+++ src/Containers/SingleItemAuctionContainer.php
@@ -4,65 +4,27 @@
 
     use Plenty\Plugin\SessionRepository;
     use Plenty\Plugin\Templates\Twig;
-
-    //    use Plenty\Plugin\Log\Loggable;
+    use PluginAuctions\Controllers\VisitorCounterSessionController;
 
     class SingleItemAuctionContainer {
 
 //        use Loggable;
 
-//        private $sessionStorageService;
-//
-//        public function __construct(SessionStorageService $sessionStorageService)
-//        {
-//            $this -> sessionStorageService = $sessionStorageService;
-//        }
-//
+        private $visitorCounterSessionController;
+
+        public function __construct(VisitorCounterSessionController $visitorCounterSessionController)
+        {
+            $this -> visitorCounterSessionController = $visitorCounterSessionController;
+        }
+
         public function call(Twig $twig, $arg) : string
         {
             $itemId = $arg[0]['item']['id'];
 
-
-            // sessionstorage (Service) abfragen
-//            $session = pluginApp(SessionStorageService::class);
-            $sessionRepo = pluginApp(SessionRepository::class);
-
-            $repoTestEins = $sessionRepo -> get("testEins"); // test
-
-            $sessionRepo -> set("itemIdArray", [$itemId]);
-//            if ($repoTestEins != $itemId)
-//            {
-//                $sessionRepo -> set("testEins", $itemId);
-////                $sessionRepo -> set("itemIdArray", $itemId);
-////                $sessionRepo -> set("prependTest", 1);
-//            }
-//            $repoTestEins += $repoTestEins;
-//            $visitorCounter = $sessionRepo -> get("testEins");
-            $sessionRepo -> push("itemIdArray", $itemId);
-//            $prependTest += $sessionRepo -> get("prependTest");
-
-//            $sessionRepo -> prepend("prependTest", 1);
-//            $this -> getLogger(__METHOD__)
-//                  -> setReferenceType('auctionId')
-//                  -> setReferenceValue($itemId)
-//                  -> debug('PluginAuctions::auctions.debug', ['prependTest: ' => $prependTest]);
-
-//            $sessionRepo -> push("itemIdArray", 0);
-//            $this -> getLogger(__METHOD__)
-//                  -> setReferenceType('auctionId')
-//                  -> setReferenceValue($itemId)
-//                  -> debug('PluginAuctions::auctions.debug', ['itemIdArray: ' => $itemIdArray]);
-//
-//            $visitorCounter = $sessionRepo -> get("testEins");
-//            $this -> getLogger(__METHOD__)
-//                  -> setReferenceType('auctionId')
-//                  -> setReferenceValue($itemId)
-//                  -> debug('PluginAuctions::auctions.debug', ['testEins: ' => $testEins]);
-
+            $itemIdArray = $this -> visitorCounterSessionController -> getItemArray($itemId);
 
             return $twig -> render('PluginAuctions::Containers.SingleItemAuction', ["itemData"       => $arg[0],
-                                                                                    "visitorCounter" => $sessionRepo -> get("testEins"),
-                                                                                    "itemIdArray"       => $sessionRepo -> get("itemIdArray")
+                                                                                    "itemIdArray"    => $itemIdArray
             ]);
         }
 
--- src/Controllers/VisitorCounterSessionController.php
+++ src/Controllers/VisitorCounterSessionController.php
@@ -4,7 +4,7 @@
 
     use Plenty\Plugin\Controller;
     use Plenty\Plugin\Log\Loggable;
-
+    use Plenty\Plugin\SessionRepository;
     use PluginAuctions\Services\Database\VisitorCounterService;
 
 
@@ -13,10 +13,55 @@
         use Loggable;
 
         private $visitorCounterService;
+        private $sessionRepo;
 
         public function __construct(VisitorCounterService $visitorCounterService)
         {
             $this -> visitorCounterService = $visitorCounterService;
+
+//            $this -> sessionRepo = pluginApp(SessionRepository::class);
+
+        }
+
+        public function getItemArray(int $itemId)
+        {
+
+            // sessionstorage (Service) abfragen
+//            $session = pluginApp(SessionStorageService::class);
+            $sessionRepo = pluginApp(SessionRepository::class);
+
+
+            $sessionRepo -> set("itemIdArray", [$itemId]);
+//            if ($repoTestEins != $itemId)
+//            {
+//                $sessionRepo -> set("testEins", $itemId);
+////                $sessionRepo -> set("itemIdArray", $itemId);
+////                $sessionRepo -> set("prependTest", 1);
+//            }
+//            $repoTestEins += $repoTestEins;
+//            $visitorCounter = $sessionRepo -> get("testEins");
+            $sessionRepo -> push("itemIdArray", $itemId);
+//            $prependTest += $sessionRepo -> get("prependTest");
+
+//            $sessionRepo -> prepend("prependTest", 1);
+//            $this -> getLogger(__METHOD__)
+//                  -> setReferenceType('auctionId')
+//                  -> setReferenceValue($itemId)
+//                  -> debug('PluginAuctions::auctions.debug', ['prependTest: ' => $prependTest]);
+
+//            $sessionRepo -> push("itemIdArray", 0);
+//            $this -> getLogger(__METHOD__)
+//                  -> setReferenceType('auctionId')
+//                  -> setReferenceValue($itemId)
+//                  -> debug('PluginAuctions::auctions.debug', ['itemIdArray: ' => $itemIdArray]);
+//
+//            $visitorCounter = $sessionRepo -> get("testEins");
+//            $this -> getLogger(__METHOD__)
+//                  -> setReferenceType('auctionId')
+//                  -> setReferenceValue($itemId)
+//                  -> debug('PluginAuctions::auctions.debug', ['testEins: ' => $testEins]);
+
+            return $sessionRepo -> get("itemIdArray");
         }
 
     }
