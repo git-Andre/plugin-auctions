e81704ccbabbf0924322dc780fd9dd59addeee3e
git-Andre <post@andre-oelschlaegel.de>
2017-10-23 02:43:03 +0200

    session new architectur 31

--- src/Containers/SingleItemAuctionContainer.php
+++ src/Containers/SingleItemAuctionContainer.php
@@ -26,6 +26,8 @@
 //
 //            $itemIdArray = $visitorCounterSessionController -> getItemArray($itemId);
 
+            $itemIdArray = ['hi ','Andrè', 'first'];
+
             return $twig -> render('PluginAuctions::Containers.SingleItemAuction', ["itemData"       => $arg[0],
                                                                                     "itemIdArray"    => $itemIdArray
             ]);
