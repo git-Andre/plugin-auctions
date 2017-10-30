80a57a239308fecf5128ebffe46c40837922b64b
git-Andre <post@andre-oelschlaegel.de>
2017-10-30 15:07:42 +0100

    auctions for itemids post

--- src/Providers/PluginAuctionsRouteServiceProvider.php
+++ src/Providers/PluginAuctionsRouteServiceProvider.php
@@ -54,7 +54,7 @@
 
             $router -> get('api/auctions', 'PluginAuctions\Controllers\AuctionsController@getAuctions');
 
-            $router -> get('api/auctions-itemids', 'PluginAuctions\Controllers\AuctionsController@getAuctionsForItemIds');
+            $router -> post('api/auctions-itemids', 'PluginAuctions\Controllers\AuctionsController@getAuctionsForItemIds');
 
             $router -> get('api/auctionshelper', 'PluginAuctions\Controllers\AuctionsController@getAuctionsHelper');
 
