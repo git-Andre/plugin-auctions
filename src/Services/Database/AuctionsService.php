ca86d1a39832ce5bacc9ee09a3c17d945830ae7b
git-Andre <post@andre-oelschlaegel.de>
2017-11-23 23:47:31 +0100

    deleteLastBid object weg

--- src/Services/Database/AuctionsService.php
+++ src/Services/Database/AuctionsService.php
@@ -620,12 +620,12 @@
                 {
                     $bidderList = array (pluginApp(AuctionBidderListEntry::class));
 
-                    $bidderList = array_pop($auction -> bidderList);
+//                    $bidderList = array_pop($auction -> bidderList);
 
                     $this -> getLogger(__METHOD__)
                           -> setReferenceType('auctionId')
                           -> setReferenceValue($auctionId)
-                          -> debug('PluginAuctions::Template.debugBefor', ['$bidderList: ' => $bidderList]);
+                          -> debug('PluginAuctions::Template.debugBefor', ['array_pop($auction -> bidderList): ' => array_pop($auction -> bidderList)]);
 
                     $auction -> bidderList = $bidderList;
 
